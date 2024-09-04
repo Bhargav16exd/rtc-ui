d1d4934aac0c3fe221a16c3d4ed3f21c584da7af
Bhargav Mule <bhargavmule16@gmail.com>
2024-09-04 19:59:31 +0530

    User connection status added

--- src/pages/Sender.tsx
+++ src/pages/Sender.tsx
@@ -13,8 +13,8 @@ export default function Sender(){
     
 
     useEffect(()=>{
-        //const socket = new WebSocket('wss://rtc-core.onrender.com')
-        const socket = new WebSocket('ws://localhost:8080');
+        const socket = new WebSocket('wss://rtc-core.onrender.com')
+        //const socket = new WebSocket('ws://localhost:8080');
     
         socket.onopen = () => {
             socket.send(JSON.stringify({type:'sender'}))
